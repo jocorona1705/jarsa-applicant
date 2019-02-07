b23483746657969ba94a40d749aed5edb704d507
@joseivan.coronado79 <joseivan.coronado79@gmail.com>
2019-02-07 10:28:16 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,14 +3,26 @@ Your module documentation here
 """
 
 
-class CalculatorClass(object):
+class Calculator_class(object):
     """
     Your class documentation here
     """
 
-    def sum(self, num_list):
-        """
-        Your method documentation here
-        """
+def suma(num_list):
+    """
+    Your method documentation here
+    """
         # your sum code here
-        return "not implement yet"  # Remove this dummy line
+    result = 0
+    for num in range(0,len(num_list)):
+        result = result + num
+    return result
+
+def inicio():
+    list = []
+    for bucle in range(0,5):
+        num = int(input('Ingrese un numero:'))
+        list.append(num)
+    result = suma(list)
+    print(result)
+if __name__ == '__main__': inicio()
